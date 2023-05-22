f3d9d9aef21efff267e100e59771c58f1d808739
Ayush <ayushz5716@gmail.com>
2023-05-23 01:06:07 +0530

    some additions

--- billCalculator.java
+++ billCalculator.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class billCalculator extends Exception{
 
     static float discountedPrice=0;
-    static String discName = "";
+    static String discName = "no discount applicable";
     static float discAmount = 0;
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
